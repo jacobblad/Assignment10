f4b1b1afdf714d0bce9693170dd5d1491ed2fd87
jacobblad <66271471+jacobblad@users.noreply.github.com>
2021-04-01 21:30:47 -0600

    Add project files.

---
+++ Components/TeamNameViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment10.Components
+{
+    public class TeamNameViewComponent : ViewComponent
+    {
+        private BowlingLeagueContext context;
+        public TeamNameViewComponent (BowlingLeagueContext ctx)
+        {
+            context = ctx;
+        }
+        
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedType = RouteData?.Values["teamname"];
+
+            return View(context.Teams
+                .Distinct()
+                .OrderBy(x => x)
+                .ToList());
+        }
+    }
+}
